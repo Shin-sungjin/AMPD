c5cb8c775ce342283584fac49fe2d4c887c8bea3
sjshin <tlstdjwls0906@gmail.com>
2024-11-03 18:06:18 +0900

    first

---
+++ src/main/java/com/example/ampd/AmpdApplication.java
@@ -0,0 +1,13 @@
+package com.example.ampd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ampd/AmpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ampd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
